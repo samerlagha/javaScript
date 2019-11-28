4f9ec1ccda3cee297a21b3b94f4159033e8ed5b3
unknown <you@example.com>
2019-11-28 17:38:19 +0200

    commit

--- lesson7/hw1/index.js
+++ lesson7/hw1/index.js
@@ -1,3 +1,7 @@
-const squareArray =  arr =>arr.filter(el => Number.isInteger(el)).map(el => Math.pow(el, 2));
+
+const squareArray = array => {
+    if (!Array.isArray(array)) return null;
+    return array.map(number => number * number);
+}
 const arr = [2,3,4] ;
  console.log(squareArray(arr));
