5ae91ce53d5594a656b36f792a468db761ce55f9
unknown <you@example.com>
2019-11-28 14:09:03 +0200

    commit

--- lesson7/task2/index.js
+++ lesson7/task2/index.js
@@ -1,6 +1,6 @@
 const sortDesc = numbers =>{
     const copy = numbers.slice();   
-     return copy.sort((a,b) => a - b );;
+     return copy.sort((a,b) => b - a );;
 };
 
 const arr= [2,1,6,3,9,8,19,11];
