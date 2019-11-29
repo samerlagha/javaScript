6427fe67a2d633409149eda61ab1153af2f6fe97
unknown <you@example.com>
2019-11-29 10:57:20 +0200

    commit

---
+++ lesson8/hw1/index.js
@@ -0,0 +1,15 @@
+function pickProps(obj,arr){
+    return arr.reduce((arr, elm) => {
+        for (const key in obj) {
+            if (key === elm) {
+                arr[elm] = obj[key];
+                break;
+            }
+        }
+        return arr;
+    }, {});
+}
+
+const arr= ['a', 17.1, 'John Doe'] ;
+const obj= { a: 'a', '17.1': 17.1, 'John Doe': 'John Doe' }
+console.log(pickProps(obj,arr));
