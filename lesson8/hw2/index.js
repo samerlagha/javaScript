88b2d440e7a016baa4c53f714c8228c1387b76b8
unknown <you@example.com>
2019-11-29 11:16:48 +0200

    commit

---
+++ lesson8/hw2/index.js
@@ -0,0 +1,15 @@
+function  getAdults(obj){
+    let adult = {};
+    
+     for(let key in obj){
+       if(obj[key] >=18){
+         adult[key] =obj[key];
+       }
+     }
+      return adult;
+      
+    }
+    
+    const obj= { 'John Doe': 19, 'Tom': 17, 'Bob': 18 };
+    
+    console.log(getAdults(obj));
