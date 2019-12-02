d44d185d054a06f2d9929ff4e8cbae7fa0af7107
unknown <you@example.com>
2019-12-02 09:07:46 +0200

    commit

---
+++ lesson10/task2/index.js
@@ -0,0 +1,11 @@
+const getParsedIntegers = arr => arr
+      .map(el=> Number.parseInt(el));
+
+const getParsedIntegersV2 = arr => arr
+    .map(el=> parseInt(el));
+
+const ggetParsedFloats = arr => arr
+.map(el=>  Number.parseFloat(el));
+
+const ggetParsedFloatsV2 = arr => arr
+.map(el=> parseFloat(el));
