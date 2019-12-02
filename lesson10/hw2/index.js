0c0719af8a7142a51f19ebac68229cb9ac15d949
unknown <you@example.com>
2019-12-02 12:41:42 +0200

    commit

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,21 +1,3 @@
-function getRandomNumbers(arr, from, to) {
-    const arry = [];
-
-      if (to < from || (to - from < 1) && Math.ceil(to) === Math.ceil(from)) {
-        return null;
-    }
-      function getRandomInt(from, to) {
-    fromn = Math.ceil(from);
-    to = Math.floor(to);
-    return Math.floor(Math.random() * (to - from)) + from;
-}
-    if (to < from || (to - from < 1) && Math.ceil(to) === Math.ceil(from)) {
-        return null;
-    }
-    for (let i = 0; i < arr; i++) {
-        arry.push(getRandomInt(from, to))
-    };
-    return arry;
-
-  
-};
+const getRandomNumbers = (array, from, to) => Array(array)
+        .fill()
+        .map(el => Math.trunc((Math.random() * (to - from) + from)));
