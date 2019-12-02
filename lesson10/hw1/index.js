c2433f7de00443353c15a6417bebf6032d94ac75
unknown <you@example.com>
2019-12-02 11:36:37 +0200

    update

--- lesson10/hw1/index.js
+++ lesson10/hw1/index.js
@@ -1,10 +1,14 @@
 const getTotalPrice = arr =>{
 
     let sum=0;
+
+    sum = arr.reduce((accumulator, currentValue) => {
+        return accumulator +=currentValue; 
+              });
   
-    for(let i=0 ; i < arr.length; i++){
-        sum += arr[i];
-    }
+    // for(let i=0 ; i < arr.length; i++){
+    //     sum += arr[i];
+    // }
     //  const res = Math.floor(sum*100)/100;
   
    return '$'+ Math.floor(sum*100)/100;
