224e73ad9380bb3af0e327433bd84729fadaefd6
unknown <you@example.com>
2019-12-02 18:08:09 +0200

    commit

--- lesson11/task2/index.js
+++ lesson11/task2/index.js
@@ -1,18 +1,30 @@
-const sortContacts =(contacts,boolen) =>{
-    
-    if(!Array.isArray(contacts)){
-        return null;
-    }
-
-    const result= contacts 
-      .sort((a,b) => {
-          if(boolen ===true || boolen ===undefined){
-
+const sortContacts = (contacts, isAsc = true) => {
+    if (!Array.isArray(contacts)) return null;
+    return contacts.sort((a, b) => {
+        if (isAsc) {
             return a.name.localeCompare(b.name);
-          } else {
-         return b.name.localeCompare(a.name);
-          }
-      
-       } ) ;
-       return result;
+        } else {
+            return b.name.localeCompare(a.name);
+        }
+    });
 };
+
+
+// const sortContacts =(contacts,boolen) =>{
+    
+//     if(!Array.isArray(contacts)){
+//         return null;
+//     }
+
+//     const result= contacts 
+//       .sort((a,b) => {
+//           if(boolen ===true || boolen ===undefined){
+
+//             return a.name.localeCompare(b.name);
+//           } else {
+//          return b.name.localeCompare(a.name);
+//           }
+      
+//        } ) ;
+//        return result;
+// };
