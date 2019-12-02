3c09c40894ff4e2a919fb02650bbaa8ed01d2bc9
unknown <you@example.com>
2019-12-02 10:32:11 +0200

    commit

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -9,9 +9,11 @@ const  getMaxAbsoluteNumber = arr =>{
     // });
     // return max ;
 
-    const absoluteValues= arr
+    const absoluteValues= arr=>{
+        if (!Array.isArray(arr) || arr.length === 0) return null;
+         arr
       .map(num => Math.abs(num));
       const max= Math.max(...absoluteValues);
      return max;
-
+    }
 }
