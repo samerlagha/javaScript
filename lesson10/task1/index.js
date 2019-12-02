c1b3976ab82df7c839d4589cd5db59b32444c4cc
unknown <you@example.com>
2019-12-02 08:46:44 +0200

    commit

---
+++ lesson10/task1/index.js
@@ -0,0 +1,18 @@
+const  getFiniteNumbers = arr => arr
+        .filter(el=> Number.isFinite(el));
+
+const getFiniteNumbersV2  = arr =>arr
+       .filter(el=>  isFinite(el))    ;   
+
+const getNaN = arr => arr
+     .filter(el => Nuumber.isNaN(el)) ;
+
+     const getNaNV2 = arr => arr
+     .filter(el => isNaN(el)) ;
+
+    const getIntegers = arr =>arr
+         .filter(el => Number.isInteger(el));
+
+        const elementList=[4,5.6, '  5.7kl',null,Infinity,undefined];
+
+        console.log(getFiniteNumbers(elementList));
