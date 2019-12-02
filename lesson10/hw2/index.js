63ba6753d4467bee11c1c64669ab5f9714e81e5c
unknown <you@example.com>
2019-12-02 12:48:58 +0200

    commit

--- lesson10/hw2/index.js
+++ lesson10/hw2/index.js
@@ -1,3 +1,13 @@
-const getRandomNumbers = (array, from, to) => Array(array)
-        .fill()
-        .map(el => Math.trunc((Math.random() * (to - from) + from)));
+const getRandomNumbers = (array, from, to) =>{
+    const wrongNumbers = to < from;
+       const noNumbersBetween = (to - from < 1) && Math.ceil(to) === Math.ceil(from);
+   
+       if (wrongNumbers || noNumbersBetween) return null;
+   
+   
+      return Array(array)
+           .fill()
+           .map(el => Math.trunc((Math.random() * (to - from) + from)));
+   
+   
+   };
