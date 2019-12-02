5224d38fd2a0bf65a70e255d93d22e0514f58403
unknown <you@example.com>
2019-12-02 17:22:24 +0200

    commit

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -9,15 +9,15 @@ if (len === undefined) {
     len= 10;
 }
    while(true){
-       let chunk =text.substr(0,len);
+       let chunk =text.substr(startPosition,len);
        if(chunk.length ===0){
 
         break;
      }
-     strArr.push(chunk[0].toUpperCase( )+ chunk.slice(1));
-      startPosition +=len;
 
-    
+     startPosition +=len;
+
+     strArr.push(chunk[0].toUpperCase( )+ chunk.slice(1));
      
      return strArr.join('\n');
    }
