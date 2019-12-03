323c141fae7a2af21ef0377a61295128be8d267c
unknown <you@example.com>
2019-12-03 12:43:18 +0200

    commit

---
+++ lesson13/task1/index.test.js
@@ -0,0 +1,15 @@
+it('17 === 17', () => {
+    expect(17).toEqual(17);
+});
+
+it('18 !== 17', () => {
+    expect(18).not.toEqual(17);
+});
+
+const getEvenNumbers = numders => numders.filter(num => num % 2 === 0);
+
+it('should get only even numbers from array', () => {
+    const result = getEvenNumbers([1, 2, 3, 4, 5, 6, ]);
+
+    expect(result).toEqual([2, 4, 6]);
+});
