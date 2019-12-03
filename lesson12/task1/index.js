3c785c5c89dc4a7cef1df9b5b2c22e8ef6fa6661
unknown <you@example.com>
2019-12-03 09:48:57 +0200

    commit

---
+++ lesson12/task1/index.js
@@ -0,0 +1,23 @@
+const calc = expression =>{
+    const [a ,operator,b]= expression.split(' ');
+    let result
+    switch(operator){
+
+        case '+':
+            result= Number(a) + Number(b);
+            break
+
+            case '-':
+            rresult= a - b;
+            break
+
+            case '*':
+            result = a * b;
+            break;
+
+            case '/':
+           result= a/ b;
+    }
+
+    return `${expression} = ${result}`;
+}
