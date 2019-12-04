54310d42853c68527adf43b0f7cf4a96ded98774
unknown <you@example.com>
2019-12-04 12:58:18 +0200

    commit

---
+++ lesson14/task1/index.js
@@ -0,0 +1,7 @@
+const message='Just learn it!';
+const sendMessage =name =>{
+    const sender ='Gromcode';
+    console.log(`${name}, ${message}. You ${sender}`);
+}
+
+const setMassage = text => message = text;
