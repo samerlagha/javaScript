fa71b0a254736fd3efe796f7afdc6eaf4045c53a
unknown <you@example.com>
2019-12-04 14:00:19 +0200

    commit

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -1,13 +1,14 @@
 'use strict';
 
 let message = "Just learn it!";
+
 export function sendMessage (name){
 	const sender = "Gromcode";
-	console.log(`${name}, ${message} Your ${sender}`);
-}
+	console.log(`${name}, ${message}. Your ${sender}`);
+};
 export function setMessage(text){
 	message = text;
-}
+};
 
 sendMessage('Ann');
 setMessage('Good job');
