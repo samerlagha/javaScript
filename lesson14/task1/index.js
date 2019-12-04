08fdb4536d554418fd7813c3c2d536a2bc964a67
unknown <you@example.com>
2019-12-04 14:11:56 +0200

    commit

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -3,7 +3,7 @@
 let message = "Just learn it!";
 export function sendMessage (name){
 	let sender = "Gromcode";
-	console.log(`${name}, ${message} Your ${sender}`);
+	console.log(`${name},  ${message} Your ${sender}`);
 }
 export function setMessage(text){
 	message = text;
