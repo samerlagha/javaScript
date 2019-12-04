0b012e9efdf1998041e3797b36bfe405be5ea112
unknown <you@example.com>
2019-12-04 17:40:03 +0200

    commit

--- lesson15/task1/index.test.js
+++ lesson15/task1/index.test.js
@@ -1,1 +1,31 @@
 import {makeCounter} from './index.js';
+it('should add number', () => {
+    const counter1 = makeCounter();
+    
+    expect(count).toEqual(0);
+});
+
+it('should add number', () => {
+    const counter1 = makeCounter();
+
+    expect(count).toEqual(1);
+});
+
+it('should add number', () => {
+    const counter2 = makeCounter();
+
+    expect(count).toEqual(0);
+});
+
+it('should add number', () => {
+    const counter1 = makeCounter();
+
+    expect(count).toEqual(2);
+});
+
+
+it('should add number', () => {
+    const counter2 = makeCounter();
+
+    expect(count).toEqual(1);
+});
