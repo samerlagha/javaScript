c47180540101430f8174e5369b1915dc28c3f282
unknown <you@example.com>
2019-12-04 15:41:16 +0200

    commit

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -1,25 +1,23 @@
-  export default  createMessenger = () =>{
-
-    let sender ='Gromcode';
+export default function createMessenger() {
+    let sender = 'Gromcode';
     let message = 'Just learn it!';
 
-    function sendMessage(name){
-     
+    function sendMessage(name) {
         console.log(`${name}, ${message} Your ${sender}`);
     }
 
     function setMessage(text) {
         message = text;
     }
-    function setSender(sender2){
-        sender = sender2;
 
+    function setSender(sender2) {
+        sender = sender2;
     }
 
-    return{
+    return {
         sendMessage,
-         setMessage,
-         setSender,
-    };
-};
+        setMessage,
+        setSender,
+    }
+}
 
