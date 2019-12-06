4631912443d783128ad7979d3001d9e059ab5614
unknown <you@example.com>
2019-12-06 10:17:45 +0200

    commit

---
+++ lesson18/task2/index.js
@@ -0,0 +1,13 @@
+ export const wallet ={
+    transactions:[1,5,89,377,3],
+    getMax(){
+//    return Math.max.apply(null,this.transaction)
+      return Math.min(...this.transactions);
+    },
+
+    getMin(){
+
+        // return Math.min.apply(null, this.transaction)
+        return Math.min(...this.transactions);
+    }
+}
