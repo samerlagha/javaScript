47150934714294f6bc262727ab27af17220cab53
unknown <you@example.com>
2019-12-09 15:47:06 +0200

    commit

---
+++ lesson20/hw2/index.js
@@ -0,0 +1,33 @@
+class User{
+
+    constructor(id,name,sessionId){
+        this._id=id;
+        this._name=name;
+        this._sessionId = sessionId;
+    }
+}
+
+
+  class UserRepository{
+
+    constructor(arr){
+        this._users= Object.freeze(arr);
+    }
+
+    getUserNames(){
+        return this._users.map(elem => elem => elem._name);
+    }
+
+    getUserIds(){
+        return this._users.map(elem => elem => elem._id);
+    }
+
+    getUserNameById(userId){
+
+    let findById = this._users.find(elem => elem._id ===userId);
+    return findById === undefined ? [] : findById._name;
+
+    }
+}
+
+export{User, UserRepository}
