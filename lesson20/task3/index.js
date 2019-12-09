3f33ab780060b07503fb91f012dd80e795cbb800
unknown <you@example.com>
2019-12-09 13:57:41 +0200

    commit

---
+++ lesson20/task3/index.js
@@ -0,0 +1,24 @@
+ export class Wallet {
+    constructor(){
+    this.balance =0;
+    }
+    getBalance(){
+      return this.balance;
+
+    }
+
+    deposit(amount){
+ this.balance += amount;
+
+    }
+
+    withdraw(aomunt){
+
+    if(amount > this.balance){
+        console.log(' No enough funds');
+        return;
+    }
+    this.balance -= amount;
+ 
+    }
+}
