2c95856b1f79ecc28473011ced00591563403ee6
unknown <you@example.com>
2019-12-09 16:03:49 +0200

    commit

--- lesson20/hw2/index.js
+++ lesson20/hw2/index.js
@@ -1,28 +1,28 @@
 class User {
     constructor(id, name, sessionId) {
-        this._id = `${id}`;
-        this._name = `${name}`;
-        this._sessionId = `${sessionId}`;
-    };
-};
+        this._id = id;
+        this._name = name;
+        this._sessionId = sessionId;
+    }
+}
 
 class UserRepository {
     constructor(arr) {
         this._users = Object.freeze(arr);
-    };
+    }
 
     getUserNames() {
         return this._users.map(el => el._name);
-    };
+    }
 
     getUserIds() {
         return this._users.map(el => el._id);
-    };
+    }
 
     getUserNameById(userId) {
         let findById = this._users.find(el => el._id === userId);
         return findById === undefined ? [] : findById._name;
-    };
-};
+    }
+}
 
 export { User, UserRepository };
