fa379102946b807234448bf3b507be038f767619
unknown <you@example.com>
2019-12-09 11:01:49 +0200

    commit

--- lesson19/task2/index.js
+++ lesson19/task2/index.js
@@ -1,11 +1,9 @@
-export  const getOwnProps = obj =>{
-       const arrOwnProps=[];
-       for(let prop in obj){
-        
-        if(obj.hasOwnProprety(prop) && typeof obj[prop]!=='function'){
-            arrOwnProps.push(prop);
+export function getOwnProps(obj) {
+    let arr = [];
+    for (let prop in obj) {
+        if (obj.hasOwnProperty(prop) && typeof(obj[prop]) != 'function') {
+            arr.push(prop);
         }
-       }
-
-       return arrOwnProps;
-}
+    }
+    return arr;
+};
