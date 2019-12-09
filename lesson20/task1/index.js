fb81324bc88286fa59b6d532c0cd4678f0ee966a
unknown <you@example.com>
2019-12-09 12:39:07 +0200

    commit

---
+++ lesson20/task1/index.js
@@ -0,0 +1,27 @@
+export class User{
+    constructor(name,age){
+    this.name = name;
+    this.age = age;
+}
+
+sayHi() {
+    console.log(`Hi, I am ${this.name}`);
+}
+
+requestNewPhoto () {
+    console.log(`New photo request was sent for ${this.name}`);
+}
+
+setAge(age) {
+    if (age < 0) return false;
+    this.age = age;
+    if (age >= 25) {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+    return age;
+}
+static creatEmpty(){
+    return new User('',null);
+}
+
+}
