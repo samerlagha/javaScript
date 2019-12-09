7690cfdb138dfbea098bb6de3336c1178cf0f255
unknown <you@example.com>
2019-12-09 09:30:18 +0200

    commit

---
+++ lesson19/task1/index.js
@@ -0,0 +1,23 @@
+const vehicle ={
+    name:'Argo',
+    move(){
+        
+        console.log(`${name} is moving`);
+    },
+
+    stop(){
+        console.log(`${name} stoped`);
+    }
+}
+
+const ship ={
+    startMachine(){
+        console.log(`${this.name}  lifting anchor up`);
+        ship.move();
+    },
+
+    stopMachine(){
+     console.log(`${this.name} lifting anchor down`);
+     ship.stop();
+    }
+}
