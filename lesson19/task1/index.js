1b973bcff1ef165fa28350c2abad2676dac5ab5f
unknown <you@example.com>
2019-12-09 09:56:46 +0200

    commit

--- lesson19/task1/index.js
+++ lesson19/task1/index.js
@@ -1,26 +1,25 @@
- export const vehicle ={
-    name:'Argo',
-    move(){
-        
-        console.log(`${name} is moving`);
+const vehicle = {
+    move() {
+        console.log(`${this.name} is moving`);
     },
+    stop() {
+        console.log(`${this.name} stopped`);
+    },
+}
 
-    stop(){
-        console.log(`${name} stoped`);
-    }
+
+const ship = {
+    name: 'Argo',
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`);
+        this.move();
+    },
+    stopMachine() {
+        this.stop();
+        console.log(`${this.name} lifting anchor down`);
+    },
 };
 
- export const ship ={
-    startMachine(){
-        ship.move();
-        console.log(`${this.name}  lifting anchor up`);
-        
-    },
+Object.setPrototypeOf(ship, vehicle);
 
-    stopMachine(){
-        ship.stop();
-     console.log(`${this.name} lifting anchor down`);
-     
-    },
-    __proto__:vehicle
-}
+export { vehicle, ship }
