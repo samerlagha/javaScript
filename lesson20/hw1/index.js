2f8d770bcd0783b2e41aff6f7e5c4db29b415c5a
unknown <you@example.com>
2019-12-09 14:28:48 +0200

    commit

---
+++ lesson20/hw1/index.js
@@ -0,0 +1,31 @@
+export class Order {
+
+    constructor(price, city, type){
+
+        this.id =`${Math.random()}`;
+        this.price=price;
+        this.dateCreated= new Date();
+        this.isConfirmed=false;
+        this.dateConfirmed=null;
+        this.city=city;
+        this.type=type
+
+    }
+
+    checkPrice(){
+        if(this.price >1000) return true;
+         return false;
+    }
+
+    confirmOrder() {
+        this.isConfirmed = true;
+        this.dateConfirmed=new Date();
+
+    }
+
+    isValidType(){
+        if (this.type == 'Buy' || this.type == 'Sell') return true;
+        return false;
+    }
+
+    }
