81c990e9bfb8e71ebd51c2914f0d26bdef95aa19
unknown <you@example.com>
2019-12-11 13:36:17 +0200

    commit

---
+++ lesson24/hw2/index.js
@@ -0,0 +1,16 @@
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const studentsBirthDays = students =>{
+ 
+   let birthDays ={};
+   students.sort((a,b) => new Date(a.birthDate).getDate()- new Date(b.birthDate).getDate() )
+      .map(elem=>{
+
+        if(birthDays[months[new Date(elem.birthDate).getMonth()]] ===undefined){
+          birthDays[months[new Date(elem.birthDate).getMonth()]] =[];
+
+        }
+        birthDays[months[new Date(elem.birthDate).getMonth()]].push(elem.name);
+      });
+      return birthDays;
+};
---
+++ lesson24/hw3/index.js
@@ -0,0 +1,77 @@
+const todos = [
+    { text: 'Buy milk', done: false,id :0 ,date: new Date(2019, 4, 1, 0, 0, 0, 0) },
+    { text: 'Pick up Tom from airport', done: false,id:1,date: new Date(2019, 5, 1, 0, 0, 0, 0) },
+    { text: 'Visit party', done: false ,id:2,date: new Date(2019, 6, 1, 0, 0, 0, 0)},
+    { text: 'Visit doctor', done: true,id:3 ,date: new Date(2019, 7, 1, 0, 0, 0, 0)},
+    { text: 'Buy meat', done: true ,id:4,date: new Date(2018, 1, 1, 0, 0, 0, 0)},
+  ];
+
+  const listElem = document.querySelector('.list');
+  const btnElem = document.querySelector('.btn');
+  const inputElem = document.querySelector('.task-input');
+  
+  
+  const renderListItems = listItems => {
+    const listElem = document.querySelector('.list');
+
+  
+    const listItemsElems = listItems
+    .sort((a,b) => b.date -a.date)
+    .sort((a,b) => a.done - b.done)
+    .map(({ text, done,id }) => {
+  
+        const listItemElem = document.createElement('li');
+        listItemElem.classList.add('list__item');
+        listItemElem.setAttribute('id',`${id}`);
+  
+        const checkboxItem = document.createElement('input');
+        checkboxItem.setAttribute('type', 'checkbox')
+        checkboxItem.checked = done;
+        if (done) {
+            listItemElem.classList.add('list__item_done');
+        }
+        checkboxItem.classList.add('list__item-checkbox');
+  
+  
+        listItemElem.append(checkboxItem, text);
+  
+        return listItemElem;
+    });
+  
+    listElem.append(...listItemsElems);
+  };
+  
+  renderListItems(todos);
+
+
+  const checkDoneToDo= (event)=>{ 
+
+const checkTarget = event.target;
+if(checkTarget.tagName != 'INPUT') return;
+
+const getId  = todos.find(elem => elem.id === +checkTarget.parentElement.id);
+getId.done = checkTarget.checked;
+
+listElem.innerHTML ='';
+renderListItems(todos);
+
+  };
+
+  listElem.addEventListener('click',checkDoneToDo);
+
+const createNewToDo= () =>{
+  if(inputElem.value =='') return;
+  todos.unshift({
+    id: todos.length +1,
+    text: inputElem.value,
+    done:false,
+
+  });
+
+  inputElem.value='';
+  listElem.innerHTML='';
+  renderListItems(todos);
+
+};
+
+btnElem.addEventListener('click',createNewToDo);
