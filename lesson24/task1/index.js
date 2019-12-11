c8afc6ca8fa6c3e35103d75033571f15a20ccaf9
unknown <you@example.com>
2019-12-11 08:57:45 +0200

    commit

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,13 +1,9 @@
-const weekDays =[ 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
+const weekDay = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St'];
 
-  export const  dayOfWeek =(date,days) =>{
-
+ export const dayOfWeek = (date, days) => {
     const day = new Date(date).getDate();
-
     const dateInFuture = new Date(date).setDate(day + days);
-
-    return   weekDays[new Date(dateInFuture).getDay()];
+    return weekDay[new Date(dateInFuture).getDay()];
 };
 
-const result =  dayOfWeek(new Date(2019,0,1),14);
-console.log(result);
+
