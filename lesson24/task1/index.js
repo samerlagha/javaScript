80527a27320c7e998a6828bd5c30322728a73355
unknown <you@example.com>
2019-12-11 08:50:29 +0200

    update

--- lesson24/task1/index.js
+++ lesson24/task1/index.js
@@ -1,13 +1,13 @@
 const weekdays =[ 'Mo', 'Tu', 'We', 'Th', 'Fr', 'St', 'Su'];
 
-const  dayOfWeek =(date,days) =>{
+  export const  dayOfWeek =(date,days) =>{
 
     const day = new Date(date).getDate;
 
     const dateInFuture = new Date(date).setDate(day + days);
 
     return   weekdays[new Date(dateInFuture).getDay()];
-}
+};
 
 const result =  dayOfWeek(new Date(2019,0,1),14);
 console.log(result);
