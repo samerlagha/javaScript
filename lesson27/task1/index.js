372b919f088798e395cca72ab3c224867eaf0ead
unknown <you@example.com>
2020-02-17 11:59:32 +0200

    commit

--- lesson27/task1/index.js
+++ lesson27/task1/index.js
@@ -1,31 +1,15 @@
-localStorage.clear();
-localStorage.setItem('hobbies',JSON.stringify({name: 'Tom'}));
-localStorage.setItem('name',JSON.stringify('Tom'));
-localStorage.setItem('age',JSON.stringify(17));
-// console.log(JSON.parse(localStorage.getItem('hobbies')));
-
-const getLocalStorageData  = () =>{
-//  for(let i =0; i <localStorage.length; i++){
-
-//     localStorage.key(i);
-//  }
-// Object.keys(localStorage);
-// Object.values(localStorage);
-return Object.entries(localStorage)
-.reduce((acc,[key , value]) =>{
-   let newValue;
-    try {
-         newValue =JSON.parse(value);
-    } catch (error) {
-         newValue=value;
-    }
-    return {
-       
-        ...acc,
-        [key]:newValue,
-
-    };
-} ,{});
-};
-
-console.log(getLocalStorageData());
+export function getLocalStorageData() {
+    return Object.entries(localStorage)
+        .reduce((acc, [key, value]) => {
+            let newValue;
+            try {
+                newValue = JSON.parse(value);
+            } catch (e) {
+                newValue = value;
+            }
+            return {
+                ...acc,
+                [key]: newValue,
+            }
+        }, {});
+}
