b010c70ed2fa30022fc2edbb473440ed1ec52d7a
unknown <you@example.com>
2020-02-19 17:44:50 +0200

    commit

---
+++ lesson29/task1/index.js
@@ -0,0 +1,16 @@
+export const addImage = (imgSrc,callback) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt','My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const onImageLoaded = () => {
+        const {width, height} = imgElem;
+        callback(null, {width, height});
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => callback('Image load failed'));
+};
