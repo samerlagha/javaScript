bace5887cf7c0302b6568685524407f3fa3b7781
unknown <you@example.com>
2020-02-20 13:22:24 +0200

    commit

--- lesson29/hw1/index.js
+++ lesson29/hw1/index.js
@@ -1,17 +1,18 @@
-export const requestUserData =(userId , callback) =>{
-
-    if(userId == 'broken'){
-        callback(null,'Failed to load user data');
-        return ;
+const requestUserData = (userId, callback) => {
+    if (userId == 'broken') {
+        callback(null, 'Failed to load user data');
+        return;
     }
-
-    let obj ={
-        name:'John',
-        age : '17',
-        userId ,
-        email: 'userid777@example.com',
+    let object = {
+        name: 'John',
+        age: 17,
+        userId,
+        email: 'userid777@example.com'
     };
 
-    callback =obj;
-    setTimeout(callback, Math.floor(Math.random()*3 +1) *1000);
-};
+    callback = (object);
+    setTimeout(callback, Math.floor(Math.random() * 3 + 1) * 1000);
+}
+
+
+export { requestUserData };
