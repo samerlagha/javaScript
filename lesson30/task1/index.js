6dcf9b601100db5dd85c1b123faa51e038d734b8
unknown <you@example.com>
2020-02-24 11:09:27 +0200

    commit

---
+++ lesson30/task1/index.js
@@ -0,0 +1,26 @@
+//addImage With promise
+
+const addImageWithPromise = imgSrc =>{
+ const p = new Promise((resolve , reject) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const  onImageLoaded = ()=> {
+        const { width, height } = imgElem;
+        resolve({ width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+    imgElem.addEventListener('error', () => reject( new Error('Image load failed')));
+ });
+
+  return p;
+};
+
+const imgSrc='https://pngimg.com/uploads/mario/mario_PNG125.png';
+
+const resultPromise = addImageWithPromise(imgSrc);
+resultPromise.then(data => console.log(data));
