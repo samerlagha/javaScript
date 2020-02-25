7acf79c74f8fb5e1689f38142ad8e734ab0c51be
unknown <you@example.com>
2020-02-25 11:08:33 +0200

    commit

--- lesson31/task1/index.js
+++ lesson31/task1/index.js
@@ -1,33 +1,24 @@
-export const requestUserData = userId =>{
- 
-    const request = new Promise((resolve,reject) =>{
-
-        if(userId ==='broken'){
-            setTimeout(()=>{
-                reject(new Error('User not found'));
-            },500);
-          
-        } else {
-            setTimeout(()=>{
-
-              resolve({
-                  name:'Jhon',
-                  age:17,
-                  email:`${userId}` +'@example.com',
-                  userId,
-              });
-            },1000);
-         
-        }
+export const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+      if (userId == "broken") {
+        setTimeout(() => {
+          reject(new Error("User not found"));
+        }, 500);
+      } else {
+        setTimeout(() => {
+          resolve({
+            name: "John",
+            age: 17,
+            userId,
+            email: `${userId}` + "@example.com"
+          });
+        }, 1000);
+      }
     });
     return request;
-
-};
-
-
-requestUserData('userid777')
-  .then(data=>console.log(data))
+  };
   
+  requestUserData('userid777')
+  .then (data => console.log(data))
   .catch(error => console.log(error))
-
-  .finally(() =>console.log('finally'));
+  .finally(() => console.log('finally'));
