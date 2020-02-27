fa04513fd7b2d37e978d14270eaf563819bcf8db
unknown <you@example.com>
2020-02-27 13:36:25 +0200

    commit

--- lesson34/task1/src/scripts/tasksGateway.js
+++ lesson34/task1/src/scripts/tasksGateway.js
@@ -1,39 +1,41 @@
-const baseUrl = 'https://crudcrud.com/api/6c2d8ce618d345e6a22d2e5dd0572c4c/tasks';
+const baseUrl = 'https://crudcrud.com/api/5d4859d709214ce19d87ba7c4e0bac37/tasks';
 
-function mapTasks(tasks) {
-    return tasks.map(({ _id, ...rest }) => ({ ...rest, id: _id }));
-};
+const mapTasks = tasks =>
+    tasks.map(({ _id, ...rest }) => ({...rest, id: _id }))
 
-function getTasksList() {
+export const getTasksList = () => {
     return fetch(baseUrl)
         .then(response => response.json())
         .then(tasks => mapTasks(tasks))
 };
 
-function createTask(taskData) {
+export const creatTask = taskData => {
     return fetch(baseUrl, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
         },
-        body: JSON.stringify(taskData),
+        body: JSON.stringify(taskData)
     })
+
 };
 
-function updateTask(taskId, updatedTaskData) {
+
+export const updateTask = (taskId, updateTaskData) => {
     return fetch(`${baseUrl}/${taskId}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json;charset=utf-8',
         },
-        body: JSON.stringify(updatedTaskData),
+        body: JSON.stringify(updateTaskData)
     })
+
 };
 
-function deleteTask(taskId) {
+export const deleteTask = taskId => {
     return fetch(`${baseUrl}/${taskId}`, {
         method: 'DELETE',
     })
+
 };
 
-export { getTasksList, createTask, updateTask, deleteTask };
