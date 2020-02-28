314368ed31074baaa9e8b8be22b9be11bf0a5ee6
unknown <you@example.com>
2020-02-28 16:55:06 +0200

    commit

--- lesson34/hw/index.js
+++ lesson34/hw/index.js
@@ -1,39 +1,46 @@
-const baseUrl = 'https://crudcrud.com/api/5d4859d709214ce19d87ba7c4e0bac37/event';
+const baseUrl = 'https://crudcrud.com/api/5d4859d709214ce19d87ba7c4e0bac37/users';
 
-const formElem = document.querySelector('.login-form');
-const submitBtnElem = document.querySelector('.submit-button');
+const form = document.forms[0];
+
+const submitButton = document.querySelector('.submit-button');
+const errorText = document.querySelector('.error-text');
 const inputs = [...document.querySelectorAll('input')];
-const errorTextElem = document.querySelector('.error-text');
+
+form.addEventListener('input', validateFields);
+form.addEventListener('submit', submitData);
 
 
-const reportValidity = () => {
-    if (formElem.reportValidity()) {
-        submitBtnElem.disabled = false;
-    } else {
-        submitBtnElem.disabled = true;
-    }
-    errorTextElem.textContent = '';
-};
 
-const postData = event => {
+
+function submitData(event) {
     event.preventDefault();
-    const newUser = [...new FormData(formElem)]
-        .reduce((acc, [field, value]) => ({...acc, [field]: value }), {});
+    const newUser = [...new FormData(form)]
+        .reduce((acc, [field, value]) => ({ ...acc, [field]: value }), {});
 
     fetch(baseUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            },
-            body: JSON.stringify(newUser),
-        })
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(newUser),
+    })
         .then(response => response.json())
         .then(data => {
             inputs.map(elem => elem.value = '');
-            submitBtnElem.disabled = true;
+            submitButton.disabled = true;
             alert(JSON.stringify(data))
         })
         .catch(() => {
-            errorTextElem.textContent = 'Failed to create user';
+            errorText.textContent = 'Failed to create user';
         });
+};
+
+
+function validateFields() {
+    if (form.reportValidity()) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+    errorText.textContent = '';
 };
