6b5b247d7b92053b198e4fd5f8fceda9daf099eb
unknown <you@example.com>
2020-03-04 09:48:11 +0200

    commit

--- lesson36/task2/gatways.js
+++ lesson36/task2/gatways.js
@@ -1,21 +1,19 @@
-//logic speak with server
+//logic speak with server + async-await
 export const fetchUserData = async userName => {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
-    
-            if (response.ok) {
-                return await response.json()
-            }
-            throw new Error('Failed to load data')
-        ;
-};
-
-export const fetchRepositories = async url =>{
-
-    const response = await fetch(url);
+    const response = await fetch(`https://api.github.com/users/${userName}`);
+         if (response.ok) {
+             return await response.json();
+         }
+         throw new Error('Failed to load data');
    
-        if (response.ok) {
-            return await response.json()
-        }
-        throw new Error('Faild to load data');
-};
-    
+       };
+   
+   
+   export const fetchRepositories = async url => {
+     const response = await fetch(url);
+       if (response.ok) {
+         return await response.json();
+       }
+       throw new Error('Failed to load data');
+       
+     };
--- lesson36/task2/index.js
+++ lesson36/task2/index.js
@@ -1,57 +1,53 @@
-import { fetchUserData, fetchRepositories } from './gatways.js';
-import { renderUserData } from './user.js';
-import { renderRepos, cleanReposList } from './repos.js';
-import { showSpinner, hideSpinner } from './spinner.js';
+import { fetchUserData, fetchRepositories } from "./gateways.js";
+import { renderUserData } from "./user.js";
+import { renderRepos, clearRepoList } from "./repos.js";
+import { showSpinner, hideSpinner } from "./spinner.js";
 
 const defaultUser = {
-    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-    name: '',
-    location: '',
+  avatar_url: "https://avatars3.githubusercontent.com/u10001",
+  name: "",
+  location: ""
 };
-
 renderUserData(defaultUser);
 
+const showUserBtnElem = document.querySelector(".name-form__btn");
+const userNameInputElem = document.querySelector(".name-form__input");
 
-const showUserBtnElem = document.querySelector('.name-form__btn');
-const userNameInputeElem = document.querySelector('.name-form__input');
-
-const listElem = document.querySelector('.repo-list');
 
 //search by name
 const onSearchUser = async () => {
-    showSpinner();
-    cleanReposList();
-    const userName = userNameInputeElem.value;
-    //cdelam zapros
-    try{
-        const userData = await fetchUserData(userName);
-        renderUserData(userData);
-       const repos= await fetchRepositories(userData.repos_url);
-       renderRepos(reposList);
-    } catch(err){
+  showSpinner();
+  clearRepoList();
+  const userName = userNameInputElem.value;
+  try {
 
-        alert(err.message);
-    } finally{
-        hideSpinner();
-    }
-  
-    // fetchUserData(userName)
-    //     .then(userData => {
-    //         renderUserData(userData);
-    //         return userData.repos_url;
-    //     })
-    //     .then(url => fetchRepositories(url))
-    //     .then(reposList => {
-    //         renderRepos(reposList);
-    //         hideSpinner();
-    //     })
-    //     .catch(err => {
-    //         hideSpinner();
-    //         alert(err.message);
-    //     })
-    //     .finally(()=>{
-    //         hideSpinner();
-    //     })
+    const userData = await fetchUserData(userName);
+    renderUserData(userData);
+    const reposList = await fetchRepositories(userData.repos_url);
+    renderRepos(reposList);
+  } catch (err) {
+    alert(err.message);
+  } finally {
+    hideSpinner();
+  }
+
+  // fetchUserData(userName) //запрос даних користувача
+  //   .then(userData => {
+  //     //prishli danni
+  //     renderUserData(userData); //відмалювали
+  //     return userData.repos_url; //передаєм дані наст обробнику
+  //   })
+  //   .then(url => fetchRepositories(url)) // запрос за списком
+  //   .then(reposList => {
+  //     renderRepos(reposList); //відмальов список
+  //   })
+  //   .catch(err => {
+  //       alert(err.message);
+  //   })
+  //   .finally(() => {
+  //       hideSpinner();
+
+  //   })
 };
 
-showUserBtnElem.addEventListener('click', onSearchUser);
+showUserBtnElem.addEventListener("click", onSearchUser);
