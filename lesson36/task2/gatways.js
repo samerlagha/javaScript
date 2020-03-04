b191d6eebb5d2e667d354f86e8ef230c22dd53f5
unknown <you@example.com>
2020-03-04 09:53:33 +0200

    commit

--- lesson36/task2/gatways.js
+++ lesson36/task2/gatways.js
@@ -1,19 +1,15 @@
-//logic speak with server + async-await
 export const fetchUserData = async userName => {
     const response = await fetch(`https://api.github.com/users/${userName}`);
-         if (response.ok) {
-             return await response.json();
-         }
-         throw new Error('Failed to load data');
-   
-       };
-   
-   
-   export const fetchRepositories = async url => {
-     const response = await fetch(url);
-       if (response.ok) {
-         return await response.json();
-       }
-       throw new Error('Failed to load data');
-       
-     };
+    if (response.ok) {
+        return await response.json()
+    }
+    throw new Error('Failed to load data');
+};
+
+export const fetchRepositories = async url => {
+    const response = await fetch(url);
+    if (response.ok) {
+        return await response.json()
+    };
+    throw new Error('Faild to load');
+};
