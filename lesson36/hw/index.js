44207ac46afb4fc2a3b003109a16c58da4a700e1
unknown <you@example.com>
2020-03-04 10:30:42 +0200

    commit

--- lesson36/hw/index.js
+++ lesson36/hw/index.js
@@ -1,16 +1,16 @@
-export const getUsersBlogs = async arr => {
-    try {
-        let response = await arr.map(userId => fetch(`https://api.github.com//${userId}`)
+ export const  getUsersBlogs = async users => {
+    const requests = users
+        .map(userId => fetch(`https://api.github.com/users/${userId}`)
             .then(response => {
-                if (response.ok) return response.json()
+                if (response.ok) return response.json();
                 throw new Error('Failed to load data');
-            })
-            .then(user => user.blog));
-        const usersData = await Promise.all(response);
-        return usersData;
-    } catch (err) {
-        console.log(err);
-    }
+            }));
+    const usersData = await Promise.all(requests);
+    return usersData.map(user => user.blog);
 };
+
+
+getUsersBlogs(["facebook", "google"])
+.then(response => console.log(response));
 
 
