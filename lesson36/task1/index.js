9e384da13591d6f9d1e2472fced8f0fa75f6f093
unknown <you@example.com>
2020-03-04 09:04:36 +0200

    commit

---
+++ lesson36/task1/index.js
@@ -0,0 +1,24 @@
+const getUser =  async userId =>{
+    //proverka erro fo async
+    try{
+
+           //zapros data dla polutshenie data from server
+        const response =  await fetch(`https://api.github.com/users/${userId}`);
+        //proverka 
+          if(!response.ok){
+              return null;
+          }
+           //esli user find
+    const userDate = await response.json() ;
+    return userDate;
+
+    } catch(err){
+        throw new Error('Failed to fetch user');
+    }
+
+  
+};
+
+getUser('facebook')
+.then(userDate =>console.log(userDate))
+   .catch(err=>alert(err.message));
